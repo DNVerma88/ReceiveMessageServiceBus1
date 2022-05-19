af1f307808af0c364bfc64f365c6d419908acdbc
Narayan, Deep <Deep.Narayan@BakerHill.com>
2022-05-19 15:52:56 +0530

    commit changes

--- ReceiveMessageServiceBus1/Program.cs
+++ ReceiveMessageServiceBus1/Program.cs
@@ -1,4 +1,11 @@
 ﻿using ReceiveMessageServiceBus1;
 
 ReceiveMessage receiveMessage = new ReceiveMessage();
-await receiveMessage.ReceiveMessageFromTopic();
+for (int i = 0; i < 10; i++)
+{
+    await receiveMessage.ReceiveMessageFromTopic();
+    Thread.Sleep(3000);
+}
+
+Console.WriteLine("Received Message from topic subscription 1.");
+Console.ReadKey();
--- ReceiveMessageServiceBus1/ReceiveMessage.cs
+++ ReceiveMessageServiceBus1/ReceiveMessage.cs
@@ -1,33 +1,23 @@
 ﻿using Azure.Messaging.ServiceBus;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReceiveMessageServiceBus1
 {
     public class ReceiveMessage
     {
-        static string connectionString = "*****";
+        static string connectionString = "ServiceBus namespace connection";
 
-        static string topicName = "notification0";
-
-
+        static string topicName = "mytopic";
 
         public async Task ReceiveMessageFromTopic()
         {
             await using var client = new ServiceBusClient(connectionString);
             
-            ServiceBusReceiver receiver = client.CreateReceiver(topicName, "subscriber1", new ServiceBusReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete});
+            ServiceBusReceiver receiver = client.CreateReceiver(topicName, "consumer1", new ServiceBusReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete});
             
             ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
             
             string body = receivedMessage.Body.ToString();
             Console.WriteLine(body);
-
-            Console.WriteLine("Received Message from topic subscription 1.");
-            Console.ReadKey();
         }
 
     }
